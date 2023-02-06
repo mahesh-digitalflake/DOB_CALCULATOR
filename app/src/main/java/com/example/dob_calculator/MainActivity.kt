216b462c3a2e8b2bc420f7be52a54414b2074b35
mahesh_karbhar <maheshkarbhar777@gmail.com>
2023-02-06 15:04:14 +0530

    demo commit

--- app/src/main/java/com/example/dob_calculator/MainActivity.kt
+++ app/src/main/java/com/example/dob_calculator/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private var tvAgeInMinute:TextView?=null
     private var tvAgeInMonths:TextView?=null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
